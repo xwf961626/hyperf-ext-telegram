0e9823a0d5e98d4061d7daabe8b310298516a09f
wenfeng <wenfeng@git.logon8.com>
2025-09-25 18:58:18 +0800

    menus

--- src/Config.php
+++ src/Config.php
@@ -2,25 +2,20 @@
 
 namespace William\HyperfExtTelegram;
 
-use William\HyperfExtTelegram\Core\Menus;
 use Hyperf\Context\ApplicationContext;
 use Hyperf\Contract\TranslatorInterface;
+use function Hyperf\Config\config;
 
 class Config
 {
     public static function languages(): array
     {
-        return ['en', 'zh_CN'];
+        return config('telegram.languages', ['zh_CN']);
     }
 
     public static function menus(): array
     {
-        return [
-            [Menus::BALANCE_QUICK_RENT, Menus::QUOTA_PACKAGE, Menus::SMART_QUOTA],
-            [Menus::ENERGY_RENTAL, Menus::TRX_QUICK_EXCHANGE, Menus::ADDRESS_MONITOR],
-            [Menus::REAL_TIME_U_PRICE, Menus::NOTIFY_GROUP, Menus::CONTACT_SERVICE],
-            [Menus::BALANCE_RECHARGE, Menus::ENERGY_QUICK_RENT, Menus::USER_CENTER],
-        ];
+        return config('telegram.menus', []);
     }
 
     public static function messages(): array
