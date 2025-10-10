f990f9d2e9b82cb5118293dd5aea9041e88ebc0d
wenfeng <wenfeng@git.logon8.com>
2025-10-10 18:01:00 +0800

    update lock

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -15,18 +15,23 @@ namespace William\HyperfExtTelegram\Controller;
 
 use Hyperf\Cache\Cache;
 use Hyperf\HttpServer\Router\Router;
+use Hyperf\Redis\Redis;
+use Hyperf\Redis\RedisFactory;
 use Telegram\Bot\Objects\Update;
 use William\HyperfExtTelegram\Core\Instance;
+use William\HyperfExtTelegram\Helper\Logger;
 use William\HyperfExtTelegram\Model\TelegramBot;
 
 class WebhookController extends BaseController
 {
     private Cache $cache;
+    protected Redis $redis;
 
-    public function __construct(Cache $cache)
+    public function __construct(Cache $cache, RedisFactory $redisFactory)
     {
         parent::__construct();
         $this->cache = $cache;
+        $this->redis = $redisFactory->get('default');
     }
 
     public static function addRoutes()
@@ -42,13 +47,32 @@ class WebhookController extends BaseController
         if (!$bot) {
             return $this->error('Invalid botId', 401);
         }
-        try {
-            $updates = $this->request->all();
-            $instance = new Instance($bot);
-            $instance->handleUpdate(Update::make($updates));
+
+        $updates = $this->request->all();
+
+        \Hyperf\Coroutine\go(function () use ($bot, $updates) {
+            try {
+                $instance = new Instance($bot);
+                $update = Update::make($updates);
+                $lockKey = "telegram:update_lock:{$update->getChat()->id}";
+                Logger::debug("update lock: $lockKey");
+                $lockTtl = 300; // 秒，锁有效期（5分钟）
+                // 1. Redis锁防止并发重复
+                $isFirst = $this->redis->set($lockKey, 1, ['NX', 'EX' => $lockTtl]);
+                if (!$isFirst) {
+                    Logger::debug("跳过频繁的update id: {$update['update_id']}");
+                    return $this->success(['status' => 'ok']);
+                }
+                $instance->handleUpdate($update);
+            } catch (\Exception $e) {
+                return $this->error($e->getMessage());
+            } finally {
+                $this->redis->del($lockKey);
+            }
             return $this->success(['status' => 'ok']);
-        } catch (\Exception $e) {
-            return $this->error($e->getMessage());
-        }
+        });
+
+        return $this->success(['status' => 'ok']);
+
     }
 }
